8a5f3926e4891bba338bb93fc918a74ca83e48c7
RedEnergy <forwot163@gmail.com>
2016-02-17 22:16:23 +0400

    Added description for methods and implemented #count function

--- src/main/kotlin/ru/redenergy/string/StringKT.kt
+++ src/main/kotlin/ru/redenergy/string/StringKT.kt
@@ -1,8 +1,21 @@
 package ru.redenergy.string
 
+/**
+ * @param left - left part from which string would be taken
+ * @param right - right part to which string would be taken
+ * @return Returns text between left and right element or empty string if nothing is found
+ */
 fun String.between(left: String, right: String): String {
     val leftPos = indexOf(left) + left.length
     val rightPos = lastIndexOf(right)
     if(leftPos < 0 || rightPos < 0 || leftPos > rightPos) return ""
     return substring(leftPos, rightPos)
+}
+
+/**
+ * @param needle - element of string which would be counted
+ * @return Amount of elements encountered
+ */
+fun String.count(needle: String): Int {
+    return this.split(needle).count() - 1
 }
